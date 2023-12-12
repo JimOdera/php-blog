7628f45264207c8596a73f6076534ce4731c7518
JimOdera <140982021+JimOdera@users.noreply.github.com>
2023-12-12 11:29:13 +0300

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
     session_start();
     define("ROOT_URL","http://127.0.0.1:8000/");
 
-    define("DB_HOST","localhost");
-    define("DB_USER","james");
-    define("DB_PASS","passCode!2");
-    define("DB_NAME","blog");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "*");
+    define("DB_PASS", "*");
+    define("DB_NAME", "*");
